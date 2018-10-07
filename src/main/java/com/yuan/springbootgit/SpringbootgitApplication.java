5dc81f4249d2df67e2bf51718da832d533eec996
yuan <2570837397@qq.com>
2018-10-07 17:27:25 +0800

    test_branch分支中的GitController添加一个hello02方法

--- src/main/java/com/yuan/springbootgit/SpringbootgitApplication.java
+++ src/main/java/com/yuan/springbootgit/SpringbootgitApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * 组员的账号
+ */
 public class SpringbootgitApplication {
 
     public static void main(String[] args) {
